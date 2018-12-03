0c75b940c6b3abce3d28ade12b19ccbcf4cdbc2a
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-12-03 15:12:54 -0600

    Gameboard can place a ship in vertical position

--- src/scenes/Main/components/Gameboard/spec/Gameboard.test.js
+++ src/scenes/Main/components/Gameboard/spec/Gameboard.test.js
@@ -5,10 +5,16 @@ describe('Gameboard', () => {
 
 
   describe('put', () => {
-    it('Places a Ship at a specific position', () => {
+    it('Can place a Ship in horizontal position', () => {
       board.put('carrier', [0, 0]);
       for (let i = 0; i < 5; i += 1) {
         expect(board.isTaken([i, 0])).not.toBe(false);
+      }
+    });
+    it('Can place a Ship in vertical position', () => {
+      board.put('carrier', [0, 0], 'vertical');
+      for (let i = 0; i < 5; i += 1) {
+        expect(board.isTaken([0, i])).not.toBe(false);
       }
     });
   });
