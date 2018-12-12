e327642b71c6d40b854e570593b7f85858ad710e
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-12-12 12:53:33 -0600

    Isolated top display into it's own module

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -1,5 +1,6 @@
 const { HTMLElem } = require('./components/HTMLElem/HTMLElem');
 const { ButtonGrid } = require('./components/ButtonGrid/ButtonGrid');
+const { TopDisplay } = require('./components/TopDisplay/TopDisplay');
 import './style.scss';
 
 const Main = (() => {
@@ -9,14 +10,7 @@ const Main = (() => {
   const container = HTMLElem('div', ['container', 'h-100'], mainSection.node)
   const row = HTMLElem('div', ['row', 'h-100'], container.node);
   const column = HTMLElem('div', ['col', 'd-flex', 'flex-column', 'justify-content-around'], row.node);
-  const top = ((parentNode) => {
-    const container = HTMLElem('div', [], column.node);
-    const { node } = container;
-    const grid = ButtonGrid();
-    node.appendChild(grid.node);
-    return { node };
-  })(column.node);
-  top.node.id = 'top';
+  column.node.appendChild(TopDisplay.node);
   const middle = HTMLElem('div', [], column.node);
   middle.node.id = 'menu';
   const bottom = ((parentNode) => {
