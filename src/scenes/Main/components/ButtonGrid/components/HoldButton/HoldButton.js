334552d02c1e913a6e7d5b1608acbb8745ed9552
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-12-12 14:05:52 -0600

    Added method to change the icon of HoldButton

--- src/scenes/Main/components/ButtonGrid/components/HoldButton/HoldButton.js
+++ src/scenes/Main/components/ButtonGrid/components/HoldButton/HoldButton.js
@@ -1,18 +1,23 @@
 const { HTMLElem } = require('../../../HTMLElem/HTMLElem');
 
 const HoldButton = (parentNode, clickable = true) => {
-  const button = HTMLElem('i', ['fas', 'fa-circle', 'target'], parentNode);
+  let activeIconClasses = ['fas', 'fa-circle'];
+  const button = HTMLElem('i', [...activeIconClasses, 'target'], parentNode);
   const { node } = button;
   const clickFunctions = [];
+  const changeIcon = (iconClasses) => {
+    [activeIconClasses, iconClasses].forEach(classSet => classSet.forEach(cssClass => node.classList.toggle(cssClass)));
+    activeIconClasses = iconClasses;
+  };
   const onClick = () => {
-    console.log('Hello :)');
+    changeIcon(['fas', 'fa-certificate']);
   };
   const setAsClickable = () => {
     node.classList.toggle('clickable');
     node.addEventListener('click', onClick);
   };
   if (clickable) setAsClickable();
-  return { node };
+  return { changeIcon, node };
 };
 
 module.exports = {
