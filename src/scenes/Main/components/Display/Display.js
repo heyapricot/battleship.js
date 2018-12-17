2384db5b0b0dec04c8937ede9144a4a17f7479a7
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-12-17 12:35:53 -0600

    BottomDisplay can simulate attack

--- src/scenes/Main/components/Display/Display.js
+++ src/scenes/Main/components/Display/Display.js
@@ -8,9 +8,13 @@ const Display = (id) => {
   const grid = ButtonGrid(10, 10, false);
   node.appendChild(grid.node);
 
+  const isCellMarked = coordinates => ['hit', 'miss'].reduce((acc, val) => acc || grid.getCell(coordinates).node.classList.contains(val), false);
+
   const markAttack = (coordinates, cssClasses) => { grid.getCell(coordinates).setCSS(cssClasses); };
 
-  return { grid, markAttack, node };
+  return {
+    grid, isCellMarked, markAttack, node,
+  };
 }
 
 module.exports = {
--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -1,13 +1,14 @@
+import './style.scss';
+
 const { HTMLElem } = require('./components/HTMLElem/HTMLElem');
 const { Display } = require('./components/Display/Display');
 const { Gameboard } = require('./components/Gameboard/Gameboard');
 const { Menu } = require('./components/Menu/Menu');
-import './style.scss';
 
 const Main = (() => {
   const mainSection = HTMLElem('section', [], document.body);
   mainSection.node.id = 'main';
-  const container = HTMLElem('div', ['container', 'h-100'], mainSection.node)
+  const container = HTMLElem('div', ['container', 'h-100'], mainSection.node);
   const row = HTMLElem('div', ['row', 'h-100'], container.node);
   const column = HTMLElem('div', ['col', 'd-flex', 'flex-column', 'justify-content-around'], row.node);
   const displays = ['BottomDisplay', 'TopDisplay'].map(id => Display(id));
@@ -19,16 +20,19 @@ const Main = (() => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
-  }
+  };
 
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-  async function demo() {
+  async function simulateAttack() {
+    let coords = NaN;
     while (board.allSunk() === false) {
-      const coords = [getRandomInt(0, 10), getRandomInt(0, 10)];
       const cssClasses = ['fas', 'fa-circle'];
+
+      do {
+        coords = [getRandomInt(0, 10), getRandomInt(0, 10)];
+      } while (BottomDisplay.isCellMarked(coords));
+
       board.receiveAttack(coords) ? cssClasses.push('hit') : cssClasses.push('miss');
       BottomDisplay.markAttack(coords, cssClasses);
       await sleep(100);
@@ -42,12 +46,11 @@ const Main = (() => {
         locationsObj[key].forEach(coordinate => BottomDisplay.grid.getCell(coordinate).setCSS(['fas', 'fa-circle', key]));
       }
     }
-  }
+  };
+
   board.randomPlacement();
   renderShips(board.getLocations());
-  demo();
-  console.log('Cells: ');
-  console.log(board.getCells());
+  simulateAttack();
 })();
 
 
